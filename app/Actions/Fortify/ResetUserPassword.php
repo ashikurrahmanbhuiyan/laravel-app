a0514ddbffe36d418c4c2a0db302476c68559e9d
ashik <s2010376107@ru.ac.bd>
2023-09-03 16:43:11 +0600

    just cheaking

--- app/Actions/Fortify/ResetUserPassword.php
+++ app/Actions/Fortify/ResetUserPassword.php
@@ -7,8 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\ResetsUserPasswords;
 
-class ResetUserPassword implements ResetsUserPasswords
-{
+class ResetUserPassword implements ResetsUserPasswords{
     use PasswordValidationRules;
 
     /**
